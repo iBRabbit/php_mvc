ba40dbdd77d3ce41282c524e5029284eafb59b26
Felix Prima <primafelix1207@gmail.com>
2022-04-13 23:56:06 +0700

    routing

--- app/core/App.php
+++ app/core/App.php
@@ -1,7 +1,20 @@
 <?php  
 
 class App {
-    public function __construct() {
-        echo "OK!";
+    public function __construct()
+    {
+        echo "OK";
+        $url = $this -> parseURL();
+        var_dump($url);
     }
+
+    public function parseURL() {
+        if(isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/'); 
+            $url = filter_var($url, FILTER_SANITIZE_URL); // supaya url bersih dari karakter aneh
+            $url = explode('/', $url);
+            return $url;
+        }
+    }
+
 }
